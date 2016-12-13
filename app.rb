f895bb0a8687884c8fb33f8f5fb06b9e5f4682b9
psclappde2 <psclapp@sivadon.com>
2016-12-13 11:41:52 +0700

    Display salesforce.contact to index

--- app.rb
+++ app.rb
@@ -4,11 +4,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-
-get "/" do
-  erb :home
-end
-
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
@@ -16,6 +11,10 @@ end
 get "/contacts" do
   @contacts = Contact.all
   erb :index
+end
+
+get "/" do
+  erb :home
 end
 
 get "/create" do
